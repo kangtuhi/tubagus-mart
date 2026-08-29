cf97a132ba8665bf7abebba5167ab7cebe9c2653
kangtuhi <tuhialkhan@gmail.com>
2026-08-29 09:03:33 +0700

    as

--- resources/views/buat-tes/index.blade.php
+++ resources/views/buat-tes/index.blade.php
@@ -5,6 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Halaman Tes</title>
+  {{-- CSS CUSTOM ================================================================= --}}
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 20px;
+    }
+    h1 {
+      color: #333;
+    }
+  </style>
+  {{-- =============================================================================== --}}
 </head>
 <body>
   <h1>Ini adalah halaman tes</h1>
