2400ecc3e997db1539fc3edee46602cdb5e04c2e
Kang Tuhi <tuhialkhan@gmail.com>
2026-08-29 07:36:41 +0700

    test(auth): make user factory RBAC aware

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,11 +30,22 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_active' => true,
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the user's account should be inactive.
+     */
+    public function inactive(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_active' => false,
+        ]);
+    }
+
+    /**
+     * Indicate that the user's email address should be unverified.
      */
     public function unverified(): static
     {
