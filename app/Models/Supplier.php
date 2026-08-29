2a9538196d8b9a3a753ae45155fce3bbf7c9be4f
Kang Tuhi <tuhialkhan@gmail.com>
2026-08-29 08:53:44 +0700

    feat(purchasing): add supplier model

---
+++ app/Models/Supplier.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable([
+    'code',
+    'name',
+    'contact_person',
+    'email',
+    'phone',
+    'address',
+    'payment_terms',
+    'credit_limit',
+    'tax_number',
+    'is_active',
+    'notes',
+])]
+class Supplier extends Model
+{
+    public function purchaseOrders(): HasMany
+    {
+        return $this->hasMany(PurchaseOrder::class);
+    }
+}
