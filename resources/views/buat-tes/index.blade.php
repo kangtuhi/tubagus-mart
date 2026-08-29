7ea515fbcd4283bde610f574f1929ec6561b4338
kangtuhi <tuhialkhan@gmail.com>
2026-08-29 09:19:04 +0700

    kontainer

--- resources/views/buat-tes/index.blade.php
+++ resources/views/buat-tes/index.blade.php
@@ -8,27 +8,29 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-  <h1>Ini adalah halaman tes</h1>
-  <div>
-    <p>ini adalah konten halaman tes.</p>
+  <div class="container">
+    <h1>Ini adalah halaman tes</h1>
     <div>
-      <table class="table table-bordered table-striped table-hover table-responsive">
-        <tr>
-          <th>No</th>
-          <th>Nama</th>
-          <th>Umur</th>
-          <th>Aksi</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>John Doe</td>
-          <td>30</td>
-          <td>
-            <button class="btn btn-sm btn-primary">Edit</button>
-            <button class="btn btn-sm btn-danger">Hapus</button>
-          </td>
-        </tr>
-      </table>
+      <p>ini adalah konten halaman tes.</p>
+      <div>
+        <table class="table table-bordered table-striped table-hover table-responsive">
+          <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Aksi</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>John Doe</td>
+            <td>30</td>
+            <td>
+              <button class="btn btn-sm btn-primary">Edit</button>
+              <button class="btn btn-sm btn-danger">Hapus</button>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 
