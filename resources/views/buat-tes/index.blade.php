6df522e598c00e51e392f2b579a22356fa84d0d8
kangtuhi <tuhialkhan@gmail.com>
2026-08-29 09:11:21 +0700

    tabel

--- resources/views/buat-tes/index.blade.php
+++ resources/views/buat-tes/index.blade.php
@@ -16,10 +16,38 @@
     h1 {
       color: #333;
     }
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-top: 20px;
+    }
+    th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+      text-align: left;
+    }
+    th {
+      background-color: #f2f2f2;
+    }
   </style>
   {{-- =============================================================================== --}}
 </head>
 <body>
   <h1>Ini adalah halaman tes</h1>
+  <div>
+    <p>ini adalah konten halaman tes.</p>
+    <div>
+      <table>
+        <tr>
+          <th>Nama</th>
+          <th>Umur</th>
+        </tr>
+        <tr>
+          <td>John Doe</td>
+          <td>30</td>
+        </tr>
+      </table>
+    </div>
+  </div>
 </body>
 </html>
