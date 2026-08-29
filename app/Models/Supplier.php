755a3be44b5dce688237b8c4082be2dc0e783aea
Kang Tuhi <tuhialkhan@gmail.com>
2026-08-29 09:58:15 +0700

    fix(testing): enable supplier factory support

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -21,6 +22,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 ])]
 class Supplier extends Model
 {
+    use HasFactory;
+
     public function purchaseOrders(): HasMany
     {
         return $this->hasMany(PurchaseOrder::class);
