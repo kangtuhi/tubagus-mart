e557d0bdba4ce132e32e95e7d0525dd3fb65ac58
Kang Tuhi <tuhialkhan@gmail.com>
2026-09-02 23:45:13 +0700

    feat(auth): wire login and logout routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,12 @@ Route::get('/', function () {
 Route::get('/tes', function () {
     return view('buat-tes.index');
 });
+
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+Route::post('/logout', [LoginController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
 
 Route::middleware(['auth', 'active', 'permission:dashboard.view'])
     ->prefix('admin')
